df631af9a24aec560ee8e63ac2cb2302d71e8008
Min Liu <capt.liu@foxmail.com>
2018-02-10 00:42:28 +0800

    update vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,25 +2,37 @@
   <div id="app">
     <!-- <img src="./assets/logo.png"> -->
     <div class="container">
-      <div style="margin-top: 2rem; padding-bottom: 1rem;"><h1>Vue Learning</h1></div>
-      <!-- Alerts -->
-      <h3>v-for learning</h3>
-      <div style="padding: 1rem;">
-        <div v-for="brand in ['success', 'info', 'warning', 'danger']" :key="brand" class="alert" :class="'alert-' + brand">
-          <strong>Well done!</strong> You successfully read this {{brand}} alert message.
+      <nav class="navbar navbar-expand-xl navbar-dark bg-primary">
+        <a class="navbar-brand" href="#">Menu</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <!-- 使用 router-link 组件来导航. -->
+            <!-- 通过传入 `to` 属性指定链接. -->
+            <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
+            <li class="nav-item active">
+              <!-- <a class="nav-link" href="#">Home</a> -->
+              <router-link to="/" v-bind:class="'nav-link'">Home</router-link>
+            </li>
+            <li class="nav-item">
+              <router-link to="/about" v-bind:class="'nav-link'">About</router-link>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Test
+                <span class="sr-only">(current)</span>
+              </a>
+            </li>
+          </ul>
         </div>
+      </nav>
+      <div class="row">
+        <!-- 路由出口 -->
+        <!-- 路由匹配到的组件将渲染在这里 -->
+        <router-view/>
       </div>
-      <p>
-        <!-- 使用 router-link 组件来导航. -->
-        <!-- 通过传入 `to` 属性指定链接. -->
-        <!-- <router-link> 默认会被渲染成一个 `<a>` 标签 -->
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-      </p>
     </div>
-    <!-- 路由出口 -->
-    <!-- 路由匹配到的组件将渲染在这里 -->
-    <router-view/>
   </div>
 </template>
 
