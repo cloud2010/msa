17b9e39f743b0c695ecb17e46bccd3b8c944b22b
Min@Home <liumin@shmtu.edu.cn>
2018-03-11 13:03:22 +0800

    add rbac module

--- client/src/main.js
+++ client/src/main.js
@@ -1,28 +1,29 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import Vuex from 'vuex'
+import router from './router'
+import store from './store'
 import BootstrapVue from 'bootstrap-vue'
 import App from './App'
-import router from './router'
 // 挂载Vue ajax组件为第三方模块axios
 import axios from 'axios'
 Vue.prototype.$http = axios
+// 挂载RBAC权限管理模块
+import '@/rbac'
 
 Vue.use(BootstrapVue)
+// 引入状态管理模块
+Vue.use(Vuex)
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  template: '<App/>',
+  store,
   components: {
     App
-  }
+  },
+  template: '<App/>'
 })
-// new Vue({
-//   router,
-//   template: '<App/>',
-//   components: {
-//     App
-//   }
-// }).$mount('#app')
+
